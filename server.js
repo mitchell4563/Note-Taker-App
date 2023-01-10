eafd4f1d0c692958a831139ba537381482146e51
Mitchell Wood <mitchellwood11@gmail.com>
2023-01-10 04:08:58 -0600

    remade file structure

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const noteData = require('./db/db.json')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(port, () => {
